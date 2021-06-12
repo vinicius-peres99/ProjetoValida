ab70176b389459638d8538ddb56fa854875391aa
pietr <pietr@LAPTOP-9J1SLRR6>
2021-06-11 21:58:09 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace ProjetoValida
+{
+    partial class FrmValida
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.BtnVerifica = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(67, 47);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(106, 33);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Location = new System.Drawing.Point(67, 136);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(101, 33);
+            this.lblIdade.TabIndex = 1;
+            this.lblIdade.Text = "Idade:";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Location = new System.Drawing.Point(67, 226);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(122, 33);
+            this.lblSalario.TabIndex = 2;
+            this.lblSalario.Text = "Salário:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(267, 44);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(261, 40);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(267, 129);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(77, 40);
+            this.txtIdade.TabIndex = 4;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(267, 223);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(160, 40);
+            this.txtSalario.TabIndex = 5;
+            // 
+            // BtnVerifica
+            // 
+            this.BtnVerifica.Location = new System.Drawing.Point(91, 337);
+            this.BtnVerifica.Name = "BtnVerifica";
+            this.BtnVerifica.Size = new System.Drawing.Size(336, 69);
+            this.BtnVerifica.TabIndex = 6;
+            this.BtnVerifica.Text = "Verificar";
+            this.BtnVerifica.UseVisualStyleBackColor = true;
+            this.BtnVerifica.Click += new System.EventHandler(this.BtnVerifica_Click);
+            // 
+            // FrmValida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.BtnVerifica);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.lblNome);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.Name = "FrmValida";
+            this.Text = "Validação";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Button BtnVerifica;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoValida
+{
+    public partial class FrmValida : Form
+    {
+        public FrmValida()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVerifica_Click(object sender, EventArgs e)
+        {
+            int saidaIdade;
+            double saidaSalario;
+
+            if (string.IsNullOrEmpty(txtNome.Text))
+            {
+                MessageBox.Show("Não pode ser branco", "Campo Nome");
+                txtNome.Focus();
+                return;
+            }
+
+
+
+            if (int.TryParse(txtIdade.Text, out saidaIdade) == false)
+            {
+                MessageBox.Show("Digitar número inteiro", "Campo Idade");
+                txtIdade.Focus();
+                return;
+            }
+
+            if (double.TryParse(txtSalario.Text, out saidaSalario) == false)
+            {
+                MessageBox.Show("Digitar um número real", "Campo Salário");
+                txtIdade.Focus();
+                return;
+            }
+
+            MessageBox.Show("Gravado com sucesso.....", "Salvando");
+        }
+    }
+}
